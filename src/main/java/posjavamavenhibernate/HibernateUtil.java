16906d2848521d5f6e91c9f9c4386def65251f89
maxmillionsmax <maximilianocaetano@gmail.com>
2022-03-18 17:15:27 -0300

    insert,consulta,updadte,delete com hibernate

--- src/main/java/posjavamavenhibernate/HibernateUtil.java
+++ src/main/java/posjavamavenhibernate/HibernateUtil.java
@@ -5,27 +5,30 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class HibernateUtil {
-	
+
 	public static EntityManagerFactory factory = null;
-	
+
 	static {
 		init();
 	}
-	
-	private static void init(){
-		
+
+	private static void init() {
+
 		try {
-			
-			if (factory == null){
+
+			if (factory == null) {
 				factory = Persistence.createEntityManagerFactory("pos-java-maven-hibernate");
 			}
-		}catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public static EntityManager getEntityManager(){
+
+	public static EntityManager getEntityManager() {
 		return factory.createEntityManager();
 	}
-		
+
+	public static Object getPrimaryKey(Object entity) {
+		return factory.getPersistenceUnitUtil().getIdentifier(entity);
+	}
 }
--- src/test/java/posjavamavenhibernate/TesteHibernate.java
+++ src/test/java/posjavamavenhibernate/TesteHibernate.java
@@ -13,14 +13,58 @@ public class TesteHibernate {
 		DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
 		
 		UsuarioPessoa pessoa = new UsuarioPessoa();
-		pessoa.setIdade(37);
-		pessoa.setLogin("Maximiliano");
+		pessoa.setIdade(3);
+		pessoa.setLogin("Maxo");
 		pessoa.setSenha("max");
-		pessoa.setSobrenome("caetano");
-		pessoa.setEmail("maximili@g.co");
+		pessoa.setNome("max");
+		pessoa.setSobrenome("caet");
+		pessoa.setEmail("max@g.co");
 		
 		daoGeneric.salvar(pessoa);
+		System.out.println(pessoa);
 	}
+	
+	@Test
+	public void testeBuscar() {
+		DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
+		UsuarioPessoa pessoa = new UsuarioPessoa();
+		pessoa.setId(3L);
 		
+		pessoa = daoGeneric.pesquisar(pessoa);
+		
+		System.out.println(pessoa);
+	}
+	
+	@Test
+	public void testeBuscar2() {
+		DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
+		
+		UsuarioPessoa pessoa = daoGeneric.pesquisar(3L, UsuarioPessoa.class);
+		
+		
+		System.out.println(pessoa);
+	}
 
+	@Test
+	public void testeUpdate() {
+		DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
+		UsuarioPessoa pessoa = daoGeneric.pesquisar(2L, UsuarioPessoa.class);
+		pessoa.setIdade(94);
+		pessoa.setNome("name");
+		pessoa.setSenha("96");
+		
+		pessoa = daoGeneric.updateMerge(pessoa);
+		
+		System.out.println(pessoa);
+	}
+	@Test
+	public void testeDelete() {
+		
+		DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
+		
+		UsuarioPessoa pessoa = daoGeneric.pesquisar(3L, UsuarioPessoa.class);
+		
+		daoGeneric.deletarPorId(pessoa);
+		
+	}
 }
