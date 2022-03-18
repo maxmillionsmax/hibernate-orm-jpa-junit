bd10671cb257561b1e8f1b500af30c7aac472cbb
maxmillionsmax <maximilianocaetano@gmail.com>
2022-03-18 17:25:50 -0300

    Carregando lista de dados e processando

--- src/test/java/posjavamavenhibernate/TesteHibernate.java
+++ src/test/java/posjavamavenhibernate/TesteHibernate.java
@@ -1,5 +1,7 @@
 package posjavamavenhibernate;
 
+import java.util.List;
+
 import org.junit.Test;
 
 import dao.DaoGeneric;
@@ -57,14 +59,28 @@ public class TesteHibernate {
 		
 		System.out.println(pessoa);
 	}
+	
 	@Test
 	public void testeDelete() {
 		
 		DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
 		
-		UsuarioPessoa pessoa = daoGeneric.pesquisar(3L, UsuarioPessoa.class);
+		UsuarioPessoa pessoa = daoGeneric.pesquisar(4L, UsuarioPessoa.class);
 		
 		daoGeneric.deletarPorId(pessoa);
 		
+	}
+	
+	@Test
+	public void testeConsultar() {
+		
+		DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
+		
+		List<UsuarioPessoa> list = daoGeneric.listar(UsuarioPessoa.class);
+
+		for (UsuarioPessoa usuarioPessoa : list) {
+			System.out.println(usuarioPessoa);
+			System.out.println("----------------------------------");
+		}
 	}
 }
